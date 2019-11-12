5cc2f8a8bffb701870c9f7615d3def310a9ee82c
pankaj-1441 <pankaj.chauhan.1441@gmail.com>
2019-11-12 16:07:55 +0530

    Fetching from external database

--- employeeservice/src/main/java/com/stackroute/employeeservice/controller/QuestionController.java
+++ employeeservice/src/main/java/com/stackroute/employeeservice/controller/QuestionController.java
@@ -1,8 +1,10 @@
 package com.stackroute.employeeservice.controller;
 
 import com.stackroute.employeeservice.domain.Question;
+import com.stackroute.employeeservice.domain.Topic;
 import com.stackroute.employeeservice.exception.QuestionNotFoundException;
 import com.stackroute.employeeservice.service.FetchQuestionsService;
+import com.stackroute.employeeservice.service.FetchTopicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,18 +15,30 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/quiz/test/")
 public class QuestionController {
-    @Autowired
-    private FetchQuestionsService fetchQuestionsService;
 
-    public QuestionController(FetchQuestionsService fetchQuestionsService) {
+    private FetchQuestionsService fetchQuestionsService;
+    private FetchTopicService fetchTopicService;
+
+    @Autowired
+    public QuestionController(FetchQuestionsService fetchQuestionsService, FetchTopicService topicService) {
         this.fetchQuestionsService = fetchQuestionsService;
+        this.fetchTopicService = topicService;
     }
+
 
     @GetMapping("questions")
     public ResponseEntity<?> getAllQuestions() throws QuestionNotFoundException{
         ResponseEntity responseEntity;
 
         responseEntity = new ResponseEntity<List<Question>>(fetchQuestionsService.getAllQuestions(), HttpStatus.OK);
+
+        return responseEntity;
+    }
+    @GetMapping("topics")
+    public ResponseEntity<?> getAllTopics() throws QuestionNotFoundException{
+        ResponseEntity responseEntity;
+
+        responseEntity = new ResponseEntity<List<Topic>>(fetchTopicService.getAllTopics(), HttpStatus.OK);
 
         return responseEntity;
     }
--- employeeservice/src/main/java/com/stackroute/employeeservice/domain/Question.java
+++ employeeservice/src/main/java/com/stackroute/employeeservice/domain/Question.java
@@ -16,7 +16,7 @@ public class Question {
     private String id;
     private String question;
     private String answer;
-    private String[] options=new String[4];
+    private String[] choices=new String[4];
     private Difficulty difficulty;
     private int weight;
 
