3b79ac63de8bf7332be0517687b73fc35782aa00
saivishalvangala <saivishalvangala@gmail.com>
2019-11-13 09:53:46 +0530

    :tada: backend service for admin page to create test

---
+++ adminService/src/main/java/com/stackroute/adminService/service/AdminService.java
@@ -0,0 +1,10 @@
+package com.stackroute.adminService.service;
+
+
+import java.io.IOException;
+
+public interface AdminService {
+
+    public void createTest(String topic)throws IOException;
+
+}
---
+++ adminService/src/main/java/com/stackroute/adminService/service/AdminServiceImpl.java
@@ -0,0 +1,36 @@
+package com.stackroute.adminService.service;
+
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AdminServiceImpl implements AdminService{
+
+    @Override
+    public void createTest(String topic) throws IOException {
+
+        Process p;
+        try {
+            String[] cmd = { "sh", "/home/cgi/testscript.sh"};
+            p = Runtime.getRuntime().exec(cmd);
+            p.waitFor();
+            BufferedReader reader=new BufferedReader(new InputStreamReader(
+                    p.getInputStream()));
+            String line;
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
