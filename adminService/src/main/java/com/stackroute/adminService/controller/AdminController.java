de835684635919838d5ca894691716ae57c0e655
saivishalvangala <saivishalvangala@gmail.com>
2019-11-18 16:52:38 +0530

    :tada: all folders are renamed in camel casing

--- adminService/src/main/java/com/stackroute/adminService/controller/AdminController.java
+++ adminService/src/main/java/com/stackroute/adminService/controller/AdminController.java
@@ -1,6 +1,9 @@
 package com.stackroute.adminService.controller;
 
+import com.stackroute.adminService.model.Message;
 import com.stackroute.adminService.service.AdminService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +14,10 @@ import java.io.IOException;
 
 @Controller
 @RequestMapping("/quiz")
+@CrossOrigin("*")
 public class AdminController {
 
+    Logger lg = LoggerFactory.getLogger(AdminController.class);
     private AdminService adminService;
 
     @Autowired
@@ -24,7 +29,9 @@ public class AdminController {
     public ResponseEntity<?> createTest(@RequestBody String topic)throws IOException {
         ResponseEntity responseEntity;
         adminService.createTest(topic);
-        responseEntity=new ResponseEntity<String>("Test is Created", HttpStatus.CREATED);
+
+        responseEntity=new ResponseEntity<Message>(new Message("Test is Created"), HttpStatus.CREATED);
+        lg.info(responseEntity.toString());
         return responseEntity;
     }
 }
---
+++ adminService/src/main/java/com/stackroute/adminService/model/Message.java
@@ -0,0 +1,20 @@
+package com.stackroute.adminService.model;
+
+public class Message {
+    private String message;
+
+    public Message(){
+
+    }
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Message(String message) {
+        this.message = message;
+    }
+}
