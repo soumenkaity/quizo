4c7a0a60d3f6e254160edc66641a1ff9b7d0bca2
chinmay kalegaonkar <chinmay.kalegaonkar@cgi.com>
2019-11-18 17:42:54 +0530

    :truck: removed the duplicate aqefrontend folder

--- aqefrontend/src/app/hr-module/model/mapping.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Mapping } from './mapping';
-
-describe('Mapping', () => {
-  it('should create an instance', () => {
-    expect(true).toBeTruthy();
-  });
-});
--- aqefrontend/src/app/hr-module/model/question.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Question } from './question';
-
-describe('Question', () => {
-  it('should create an instance', () => {
-    expect(new Question(1,"hey","a",["a","v","c","d"],"E",1,2)).toBeTruthy();
-  });
-});
