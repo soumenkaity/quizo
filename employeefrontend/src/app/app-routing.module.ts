78e07cbc59f3a04507f299f4c84f5f24d7358574
pankaj-1441 <pankaj.chauhan.1441@gmail.com>
2019-11-18 09:47:52 +0530

    employeeregistrationdetails

--- employeefrontend/src/app/app-routing.module.ts
+++ employeefrontend/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
+ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TestInsComponent } from './test-ins/test-ins.component';
 import { TestPageComponent } from './test-page/test-page.component';
 import { ThankyouComponent } from './thankyou/thankyou.component';
 import { ErrorComponent } from './error/error.component';
+import { EmployeedetailsComponent } from './employeedetails/employeedetails.component';
 
 const routes: Routes = [
-  { path: '' , component: TestInsComponent},
+  { path: '' , component: EmployeedetailsComponent},
+  { path: 'test-instructions' , component: TestInsComponent},
   { path: 'test-page' , component: TestPageComponent},
   { path: 'error', component: ErrorComponent},
   { path: 'thankyou', component: ThankyouComponent},
@@ -20,3 +22,12 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  EmployeedetailsComponent,
+  TestInsComponent,
+  TestPageComponent,
+  ErrorComponent,
+  ThankyouComponent,
+  PageNotFoundComponent
+]
--- employeefrontend/src/app/app.module.ts
+++ employeefrontend/src/app/app.module.ts
@@ -1,28 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from './material/material.module';
 import { UserService } from './user.service';
 import {DataService} from './data.service';
-import { TestInsComponent } from './test-ins/test-ins.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { TestPageComponent } from './test-page/test-page.component';
-import { ThankyouComponent } from './thankyou/thankyou.component';
-import { ErrorComponent } from './error/error.component'
 import { TopicService } from './topic.service';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
-    TestInsComponent,
-    PageNotFoundComponent,
-    TestPageComponent,
-    ThankyouComponent,
-    ErrorComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
