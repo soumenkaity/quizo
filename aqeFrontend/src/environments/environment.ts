90d5cec678cac942a5213584e36a5a27c9f7031a
chinmay <kalegaonkar.chinmay1@gmail.com>
2019-11-26 13:54:55 +0530

    :wrench: changed routes in environment.ts

--- aqeFrontend/src/environments/environment.ts
+++ aqeFrontend/src/environments/environment.ts
@@ -5,9 +5,9 @@
 export const environment = {
   production: false,
 
-  HrURLprefix: 'http://15.206.152.181:8084/hr',
-  employeeURLprefix: 'http://15.206.152.181:8083/quiz/test',
-  adminURLprefix:'http://15.206.152.181:8082/admin',
+  HrURLprefix: 'http://15.206.152.181:8080/hrService',
+  employeeURLprefix: 'http://15.206.152.181:8080/employeeService',
+  adminURLprefix:'http://15.206.152.181:8080/adminService',
   authURLprefix:'http://15.206.152.181:8081/auth',
 };
 
