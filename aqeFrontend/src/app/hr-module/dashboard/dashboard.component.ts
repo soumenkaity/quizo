298196f73ef1968d49714d7f638c5b3e0ff2d221
chinmay <kalegaonkar.chinmay1@gmail.com>
2019-11-29 09:36:49 +0530

    made hr dashboard

--- aqeFrontend/src/app/hr-module/dashboard/dashboard.component.ts
+++ aqeFrontend/src/app/hr-module/dashboard/dashboard.component.ts
@@ -1,19 +1,8 @@
 import { Component, OnInit, ViewChild, QueryList, ViewChildren, AfterViewInit } from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
-
-const ELEMENT_DATA = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { TopicService } from '../service/topic.service';
+import { EmployeeService } from '../service/employee.service';
 
 
 @Component({
@@ -23,23 +12,29 @@ const ELEMENT_DATA = [
 })
 export class DashboardComponent implements OnInit,AfterViewInit {
 
-
-  
-
-  constructor() { }
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource1 = new MatTableDataSource(ELEMENT_DATA);
-  dataSource2 = new MatTableDataSource(ELEMENT_DATA);
+  constructor( private topicService: TopicService,private employeeService: EmployeeService) { }
+  topicDisplayedColumns: string[] = ['name', 'createdAt', 'link','keywords'];
+  employeeDisplayedColumns: string[] = [ 'name', 'phone', 'email'];
+  topicDataSource;
+  employeeDataSource;
 
   @ViewChildren(MatPaginator) paginators = new QueryList<MatPaginator>();
 
   ngOnInit() {
-    
+    this.topicService.getAllTopics().subscribe(
+      response => {
+      this.topicDataSource = new MatTableDataSource(response)
+      this.topicDataSource.paginator = this.paginators.toArray()[0];
+    });
+    this.employeeService.getAllEmployees().subscribe(
+      response => {
+      this.employeeDataSource = new MatTableDataSource(response)
+      this.employeeDataSource.paginator = this.paginators.toArray()[1];
+    }
+    )
   }
 
   ngAfterViewInit(): void {
-    this.dataSource1.paginator = this.paginators.toArray()[0];
-    this.dataSource2.paginator = this.paginators.toArray()[1];
   }
 
 }
