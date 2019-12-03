f31a2b71744ed6319c71681c1ed473a3e247c810
chinmay <kalegaonkar.chinmay1@gmail.com>
2019-12-03 16:46:56 +0530

    changed in environment.ts

--- aqeFrontend/src/app/employee-module/dashboard/dashboard.component.ts
+++ aqeFrontend/src/app/employee-module/dashboard/dashboard.component.ts
@@ -46,7 +46,7 @@ export class DashboardComponent implements OnInit {
 
   giveTest(data){
     this.dataService.setTestUserDetails(data);
-    this.router.navigate(["/employee/test-instructions"])
+  this.router.navigate(["/employee/test-instructions"])
   }
 
 }
