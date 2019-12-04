ca9890d0f38422825e0b8642b1a6afc993f7dc07
Vangala sai vishal <saivishalvangala@gmail.com>
2019-12-04 17:04:04 +0530

    :zap: including global exception and comments in admin service

--- adminService/src/main/java/com/stackroute/adminService/service/AdminServiceImpl.java
+++ adminService/src/main/java/com/stackroute/adminService/service/AdminServiceImpl.java
@@ -14,26 +14,41 @@ public class AdminServiceImpl implements AdminService{
     @Override
     public int  createTest(String topic) throws IOException {
         int value=4;
+
+        //creates a new process
         Process p;
         try {
+
+            //these commands will run the script file
             String[] cmd = { "sh", "/home/cgi/Documents/question-generation/QuestionGeneration/runscript.sh", topic};
             p = Runtime.getRuntime().exec(cmd);
 
+            //this will print the terminal logs in console during program execution
             BufferedReader reader=new BufferedReader(new InputStreamReader(
                     p.getInputStream()));
             String line;
             while((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
+
+            //this will wait for the process p to be terminated
             p.waitFor();
+
+            //the python program returns the exit value which tells about the different outputs that program gives
             value=p.exitValue();
-            System.out.println("After waitfor() exit value is : " +value);
+            System.out.println("After waitFor() exit value is : " +value);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
+            value=5;
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
+            value=6;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            value=7;
         }
         return value;
 
