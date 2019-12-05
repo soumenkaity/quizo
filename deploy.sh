d6978076bd5a70964fe2c5241db066b753ce0bde
chinmay <kalegaonkar.chinmay1@gmail.com>
2019-12-05 17:01:04 +0530

    added the correct runners

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 rm - rf adaptive-quiz-engine/
 git clone https://gitlab-ci-token:8sk7oM92YNs3sFr83AkAhttps@gitlab-cgi.stackroute.in/cgi-wave14/adaptive-quiz-engine.git
+cd adaptive-quiz-engine 
 git pull origin v1.0.0
 mvn clean compile package -DskipTests
 sudo docker-compose down
