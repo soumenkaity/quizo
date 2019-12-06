9965dda6e1f28b8dd5e335ea083ee3a6e463f462
chinmay <kalegaonkar.chinmay1@gmail.com>
2019-12-06 10:25:50 +0530

    refactored adminService to uses /admin routes in dashboard

---
+++ adminService/src/main/java/com/stackroute/adminService/controller/DashboardController.java
@@ -0,0 +1,31 @@
+package com.stackroute.adminService.controller;
+
+
+import com.stackroute.adminService.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/admin")
+public class DashboardController {
+ private DashboardService dashboardService;
+
+ @Autowired
+  public DashboardController(DashboardService dashboardService) {
+    this.dashboardService = dashboardService;
+  }
+
+  @GetMapping("/users")
+  public ResponseEntity getAllUsers(){ return new ResponseEntity(dashboardService.getAllUsers(), HttpStatus.OK);}
+  @GetMapping("/topics")
+  public ResponseEntity getAllTopics(){ return new ResponseEntity(dashboardService.getAllTopics(), HttpStatus.OK);}
+  @GetMapping("/requests")
+  public ResponseEntity getAllRequests(){ return new ResponseEntity(dashboardService.getAllRequests(), HttpStatus.OK);}
+
+}
---
+++ adminService/src/main/java/com/stackroute/adminService/repository/TopicRepository.java
@@ -0,0 +1,7 @@
+package com.stackroute.adminService.repository;
+
+import com.stackroute.adminService.model.Topic;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TopicRepository extends MongoRepository<Topic,String> {
+}
