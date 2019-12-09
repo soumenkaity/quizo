198855c9464dc9bd12b9cd3e7a21b1ac437f9c42
chinmay <kalegaonkar.chinmay1@gmail.com>
2019-12-09 14:08:24 +0530

    show result on employee side

--- aqeFrontend/src/app/employee-module/service/employeeresultservice.service.ts
+++ aqeFrontend/src/app/employee-module/service/employeeresultservice.service.ts
@@ -1,29 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { EmployeeResult } from '../model/employeeresult';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeresultserviceService {
    
-
-  url: string = 'http://localhost:8083/quiz/test/result';
-  url2: String = 'http://localhost:8083/employee/resultbyid';
+  URLprefix = environment.employeeURLprefix;
   constructor(private http: HttpClient) { }
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
     })
   };
-
-   postScore3(employeeresult:EmployeeResult)
-   {
-    return this.http.post(this.url, employeeresult);
-   }
    
-   getResult(id:String)
-   {
-     return this.http.get<any>(this.url2+'/'+id);
+   getResultForTest(id:String){
+     return this.http.get<any>(this.URLprefix+'/result'+id);
+   }
+   getResultForEmployee(empId){
+    return this.http.get<any>(this.URLprefix+'/result/employee'+empId);
+   }
+   getResultForEmployeeWhoGaveThisTest(empId,testId){
+    return this.http.get<any>(this.URLprefix+'/result?testId='+testId+'&empId='+empId);
    }
 
 }
