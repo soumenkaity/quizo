a0c208c8807e3634ef557636c1fc4edadfcd518c
chinmay <kalegaonkar.chinmay1@gmail.com>
2019-12-12 19:44:15 +0530

    changed test confirmation screen to table

--- aqeFrontend/src/app/hr-module/confirm/confirm.component.ts
+++ aqeFrontend/src/app/hr-module/confirm/confirm.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList, ViewChild } from '@angular/core';
 import { Mapping } from '../model/mapping';
 import { ConfirmService } from '../service/confirm.service';
 import { Topic } from '../model/topic';
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 import { ToasterService } from 'src/app/authentication-module/service/toaster-service.service';
 import { TestUser} from '../model/testUser';
 import * as uuid from 'uuid';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 @Component({
   selector: 'app-confirm',
   templateUrl: './confirm.component.html',
@@ -19,18 +20,24 @@ export class ConfirmComponent implements OnInit {
   private test: Topic;
   private employees: Employee[]
   private mappingList = new Array();
+  employeeDataSource: any;
 
   constructor(private confirmService: ConfirmService,
               private dataService: DataService,
               private router: Router,
-              private ts:ToasterService) 
-              {
-                
-               }
+              private ts:ToasterService) {}
 
+  employeeDisplayedColumns: string[] = ['id', 'name', 'phone', 'email'];
+
+  @ViewChildren(MatPaginator) paginators = new QueryList<MatPaginator>();
+  @ViewChild(MatSort, {static: true}) matSort: MatSort;
+  @ViewChild('empSort', {static: true}) empSort: MatSort;
   ngOnInit() {
     this.test = this.dataService.getSelectedTest();
     this.employees = this.dataService.getSelectedEmployees();
+    this.employeeDataSource = new MatTableDataSource(this.employees)
+    this.employeeDataSource.sort = this.empSort;
+    this.employeeDataSource.paginator = this.paginators.toArray()[1];
   }
 
   restart(){
@@ -59,4 +66,11 @@ export class ConfirmComponent implements OnInit {
     );
   }
 
+  applyFilterEmployee(filterValue: string) {
+    this.employeeDataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.employeeDataSource.paginator) {
+      this.employeeDataSource.paginator.firstPage();
+    }
+  }
 }
