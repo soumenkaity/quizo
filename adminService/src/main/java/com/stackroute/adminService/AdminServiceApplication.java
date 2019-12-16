e5caab666f98f91308586f7f8a79e8dd54806f47
soumenkaity <soumenkaity97@gmail.com>
2019-12-16 16:05:57 +0530

    admin service is modified

--- adminService/src/main/java/com/stackroute/adminService/AdminServiceApplication.java
+++ adminService/src/main/java/com/stackroute/adminService/AdminServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("localhost:4200")
 @SpringBootApplication
 public class AdminServiceApplication {
 
